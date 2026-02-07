a138bd24de7bf44018cefddf9556847b63be1ced
cjmot <146262493+cjmot@users.noreply.github.com>
2026-02-06 21:14:20 -0700

    add createFranchise and getUserFranchises tests

--- src/routes/routesTests/franchiseRouter.test.js
+++ src/routes/routesTests/franchiseRouter.test.js
@@ -1,42 +1,101 @@
+// javascript
 const { expectValidJwt } = require('./index.js');
 const request = require('supertest');
 const app = require('../../service');
 const { DB, Role } = require('../../database/database');
 
 describe('franchiseRouter', () => {
-    let testUser = {
-        id: '',
-        name: 'pizza diner',
-        email: 'reg@test.com',
-        password: 'a',
-    };
-    let testUserAuthToken;
-    let testAuth;
-    let testAdmin = createAdminUser();
-    let testFranchise = {
-        id: 0,
-        name: 'testFranchise',
-        admins: [{ id: 0, name: 'test franchisee', email: 'testEmail@jwt.com' }],
-        stores: [{ id: 1, name: 'SLC', totalRevenue: 0 }],
-    };
+    let testUser = {};
+    let testToken = '';
 
     beforeEach(async () => {
-        testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-        const registerRes = await request(app).post('/api/auth').send(testUser);
-        testUserAuthToken = registerRes.body.token;
-        expectValidJwt(testUserAuthToken);
-        testAuth = { Authorization: `Bearer ${testUserAuthToken}` };
-        testUser.id = registerRes.body.user.id;
+        [testUser, testToken] = await randomRegisteredUser();
+        expectValidJwt(testToken);
     });
 
-    test();
+    test('createFranchise success', async () => {
+        const testAdmin = await createAdminUser();
+        const adminToken = await loginUser(testAdmin);
+        const testFranchise = {
+            name: Math.random().toString(36).substring(2, 12) + 'TestFranchise',
+            admins: [{ email: testUser.email }],
+        };
+        // create franchise with admin user with franchiseeUser as a franchise admin
+        const createRes = await request(app)
+            .post('/api/franchise')
+            .set({ Authorization: `Bearer ${adminToken}` })
+            .send(testFranchise)
+            .expect(200);
+        expect(createRes.body).toMatchObject({
+            name: testFranchise.name,
+            admins: [
+                {
+                    email: testUser.email,
+                    id: expect.any(Number),
+                    name: testUser.name,
+                },
+            ],
+        });
+    });
+
+    test('getUserFranchises success', async () => {
+        const testAdmin = await createAdminUser();
+        const adminToken = await loginUser(testAdmin);
+        const testFranchises = [
+            {
+                name: Math.random().toString(36).substring(2, 12) + 'TestFranchise',
+                admins: [{ email: testUser.email }],
+            },
+            {
+                name: Math.random().toString(36).substring(2, 12) + 'TestFranchise2',
+                admins: [{ email: testUser.email }],
+            },
+        ];
+        for (let franchise of testFranchises) {
+            await request(app)
+                .post('/api/franchise')
+                .set({ Authorization: `Bearer ${adminToken}` })
+                .send(franchise)
+                .expect(200);
+        }
+
+        const getFranchisesRes = await request(app)
+            .get(`/api/franchise/${testUser.id}`)
+            .set({ Authorization: `Bearer ${testToken}` })
+            .expect(200);
+        expect(getFranchisesRes.body).toMatchObject(testFranchises);
+    });
 });
 
-async function createAdminUser() {
+// Create an admin user directly in DB
+async function createAdminUser(email) {
     let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
-    user.name = 'test admin';
-    user.email = user.name + '@admin.com';
+    user.name = 'testAdmin';
+    user.email = email || Math.random().toString(36).substring(2, 12) + '@admin.com';
 
     user = await DB.addUser(user);
     return { ...user, password: 'toomanysecrets' };
+}
+
+async function loginUser(user) {
+    const res = await request(app)
+        .put('/api/auth')
+        .send({ email: user.email, password: user.password })
+        .expect(200);
+    return res.body.token;
+}
+
+async function register(user) {
+    const res = await request(app).post('/api/auth').send(user).expect(200);
+    return [res.body.user, res.body.token];
+}
+
+async function randomRegisteredUser() {
+    const randomString = Math.random().toString(36).substring(2, 12);
+    const randomDiner = {
+        name: randomString + 'User',
+        email: randomString + '@testUser.com',
+        password: 'testPass',
+    };
+    return await register(randomDiner);
 }
