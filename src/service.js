7ef99d8e617ebe62fe6ed274be9794a74df06af6
cjmot <146262493+cjmot@users.noreply.github.com>
2026-03-19 13:44:33 -0600

    Add basic logging middleware to service init

--- src/service.js
+++ src/service.js
@@ -6,17 +6,27 @@ const userRouter = require('./routes/userRouter.js');
 const version = require('./version.json');
 const config = require('./config.js');
 const metrics = require('./metrics.js');
+const logger = require('./logger.js');
 
 const app = express();
 app.use(express.json());
+app.use(setAuthUser);
+
+// Metrics
 app.use(metrics.requestTracker);
 app.use(metrics.requestLatencyTracker);
-app.use(setAuthUser);
 app.use(metrics.activeUserTracker);
+
+// Logging
+app.use(logger.httpLogger);
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
